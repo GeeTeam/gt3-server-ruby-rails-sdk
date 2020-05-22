c50b27ee70c3cb7d3b7fb3ce4076b11fc536c4e3
liuquan <liuquan@geetest.com>
2020-05-22 17:28:17 +0800

    feat v3.1.0 统一各语言sdk标准

---
+++ app/controllers/geetest_config.rb
@@ -0,0 +1,4 @@
+class GeetestConfig
+  GEETEST_ID = "c9c4facd1a6feeb80802222cbb74ca8e".freeze
+  GEETEST_KEY = "e4e298788aa8c768397639deb9b249a9".freeze
+end
---
+++ app/controllers/sdk/geetest_lib_result.rb
@@ -0,0 +1,49 @@
+# sdk lib包的返回结果信息。
+class GeetestLibResult
+
+  def initialize
+    @status = 0 # 成功失败的标识码，1表示成功，0表示失败
+    @data = '' # 返回数据，json格式
+    @msg = '' # 备注信息，如异常信息等
+  end
+
+  def setAll(status, data, msg)
+    @status = status
+    @data = data
+    unless msg.nil? || msg.empty?
+      @msg = "#{@msg}; #{msg}"
+    end
+  end
+
+  def setStatus(status)
+    @status = status
+  end
+
+  def getStatus
+    @status
+  end
+
+  def setData(data)
+    @data = data
+  end
+
+  def getData
+    @data
+  end
+
+  def setMsg(msg)
+    unless msg.nil? || msg.empty?
+      @msg = "#{@msg}; #{msg}"
+    end
+  end
+
+  def getMsg
+    @msg
+  end
+
+  def to_s
+    "GeetestLibResult{status=#{@status}, data='#{@data}', msg='#{@msg}'}"
+  end
+
+end
+
